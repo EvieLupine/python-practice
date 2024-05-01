eeab3ef4ab992d98a5280cc3cd0eca32ad17960c
EvieLupine <159732957+EvieLupine@users.noreply.github.com>
2024-05-02 10:30:00 +1200

    Started to create my quiz

---
+++ main.py
@@ -0,0 +1,14 @@
+# Ask the user their name and store it. Hello what is your name? 
+input("Hello what is your name?")
+print("Lovely name lets start")
+# Introduce the quiz and what it is about.
+print("Welcome to my quiz, I will be asking serveral questions and you just have to answer!")
+
+print("This quiz is Guess The Animal! These questions will get progressively harder, it is okay if you don't get it right.")
+# Ask the user a question and get their response.
+input("What is this animal?")
+
+# Afterwards tell them the answer.
+# Repeat untill quiz is finished.
+# Leave with a sign off. 
+
