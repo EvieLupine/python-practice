13abf05dc1aaa65c3d1674aa17766cd09d297d39
EvieLupine <159732957+EvieLupine@users.noreply.github.com>
2024-05-06 12:00:04 +1200

    Addingmore

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # Ask the user their name and store it. Hello what is your name? 
-input("Hello what is your name?")
-print("Lovely name lets start")
+name = input("Hello what is your name?")
+print(name, "? What a lovely name lets start")
 # Introduce the quiz and what it is about.
 print("Welcome to my quiz, I will be asking serveral questions and you just have to answer!")
 
 print("This quiz is Guess The Animal! These questions will get progressively harder, it is okay if you don't get it right.")
 # Ask the user a question and get their response.
-input("What is this animal?")
-
-# Afterwards tell them the answer.
+input("What is this animal?" "It is a fluffy mammal, has four legs, it meows, fairly small, has whiskers and typically lives in a house.") 
+# Afterwards tell them the answer if it is wrong or right.
+if answer == "Cat" "cat":
+ print("Awesome! That's correct!")
+print("Good job but that is wrong.")
 # Repeat untill quiz is finished.
+input("Okay, what is this animal now?" "It can vary from size, has four legs, usually fluffy, barks and is usally called 'Man's Best Friend.")
+if answer == "Dog" "dog":
+ print("Awesome! That's correct!")
+print("Good job but that is wrong.")
+input("I will try make this harder now." "This animal has ")
 # Leave with a sign off. 
 
