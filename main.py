ed7467a61079a921d6c906dedce0140560038e58
EvieLupine <159732957+EvieLupine@users.noreply.github.com>
2024-05-07 15:10:19 +1200

    UPDATEDDD RAHHH

--- main.py
+++ main.py
@@ -1,21 +1,121 @@
+score = 0
 # Ask the user their name and store it. Hello what is your name? 
 name = input("Hello what is your name?")
 print(name, "? What a lovely name lets start")
 # Introduce the quiz and what it is about.
 print("Welcome to my quiz, I will be asking serveral questions and you just have to answer!")
 
-print("This quiz is Guess The Animal! These questions will get progressively harder, it is okay if you don't get it right.")
+print("This quiz is Guess The Animal! I will be asking you to guess an animal based off of it's features and adjectives used to describeit. It's okay if you don't get it right.")
 # Ask the user a question and get their response.
-input("What is this animal?" "It is a fluffy mammal, has four legs, it meows, fairly small, has whiskers and typically lives in a house.") 
 # Afterwards tell them the answer if it is wrong or right.
-if answer == "Cat" "cat":
+answer = input("What is this animal?" "It is a fluffy mammal, has four legs, it meows, fairly small, has whiskers and typically lives in a house.") 
+
+if answer == "Cat":   
  print("Awesome! That's correct!")
-print("Good job but that is wrong.")
+ score += 1
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is cat!")
 # Repeat untill quiz is finished.
 input("Okay, what is this animal now?" "It can vary from size, has four legs, usually fluffy, barks and is usally called 'Man's Best Friend.")
-if answer == "Dog" "dog":
+if answer == "Dog":   
  print("Awesome! That's correct!")
-print("Good job but that is wrong.")
-input("I will try make this harder now." "This animal has ")
-# Leave with a sign off. 
+ score += 1
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Dog!")
 
+
+input("I will try make this harder now." "This animal has a long neck, eats leaves from tall trees, covered in spots and has a purple tongue." "What animal is this?")
+if answer == "Giraffe":   
+ print("Yes good job!")
+ score += 1
+elif answer == "":  
+ print("Sorry I don't get it.")
+else:   
+ print("No sorry that isn't it.")
+print("The answer is Giraffe!")
+
+input("This animal is powerful and has two horns on the top of it's head, it has four legs with hooves and are male but often get called by another name that starts with c and they are often said to be attracted to the colour red.")
+
+# Leave with a sign off. 
+if answer == "Bull":   
+ print("Awesome! That's correct!")
+ score += 1
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Bull!")
+
+input("This animal has no fur, it is scaly, not slimy and has no legs.")
+if answer == "Snake":   
+ print("Awesome! That's correct!")
+ score += 1
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Snake!")
+
+input("This animalis fluffy, likes to swim, is the largest rodent in the world and is native to central South America.")
+if answer == "Capybara":   
+ print("Awesome! That's correct!")
+ score += 2
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Capybara!")
+
+input("This animal weighs 1,000 kgs, they are native to the arctic circle, have two very large teeth and are quite lazy." "What animal do you think it is?")
+if answer == "Walrus":   
+ print("Awesome! That's correct!")
+ score += 2
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Walrus!")
+
+input("This animal is oneof Africa's most deadly killers, it is relatively the same size as a wolf, a fun fact about them is that they are born with a full set of teeth. They also like to laugh." "What is your guess?")
+if answer == "Hyena":   
+ print("Awesome! That's correct!")
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Hyena!")
+
+input("This next animal is cheeky and lives in trees, they eat fruit, seeds and some even eat meat. They also have have hands that they use to climb with. What do you guess?")
+if answer == "Monkey":   
+ print("Awesome! That's correct!")
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Monkey!")
+
+input("This animal can be quite large and scary, it spends most of it's time waiting for its next meal in the water and has a large set of teeth, this animal is a reptile. Your guess?")
+if answer == "Alligator":   
+ print("Awesome! That's correct!")
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Alligator!")
+
+input("Smaller than a typical wolf, this animal has almost an identical resemblence but is smaller,it howls barks and makes weird noises too. It is also native to america like the Grey Wolf and is a canine. What do you think it is?")
+if answer == "Coyote":   
+ print("Awesome! That's correct!")
+elif answer == "":  
+  print ("Not sure what you meant by that?!")
+else:     
+  print("Good job but that is wrong.")
+print("The answer is Coyote!")
+
+input
