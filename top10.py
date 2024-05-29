44e1f755d1cdee5d4c17ccb07c58023e12d3f57b
EvieLupine <159732957+EvieLupine@users.noreply.github.com>
2024-05-30 10:31:47 +1200

    sdeguygufd

--- top10.py
+++ top10.py
@@ -20,6 +20,12 @@ def getLives():
                 print("Please choose a positive number.")
       except:   
           print("That's not a number!")    
+
+def isCorrect( answer, list):   
+    if answer in list:  
+        return True
+    else:   
+        return False
 #-------------- MAIN CODE ----------------
 introduction()
 lives = getLives()
@@ -27,4 +33,3 @@ score = 0
 # Start the questions 
 while lives > 0:    
     answer = input("Can you name one of the top 10 biggest bones in your body?:\n").lower()
-    
